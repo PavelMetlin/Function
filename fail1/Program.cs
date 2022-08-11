57e6611c1fea953a4891016a661991ddbbf767c5
PavelMetlin <metlin-1@mail.ru>
2022-08-11 21:15:47 +0300

    initial

--- fail1/Program.cs
+++ fail1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int FindQuarter(int x, int y);
+{
+    if (x > 0 && y > 0)
+        return 1;
+    else if (x < 0 && y > 0)
+        return 2;
+    else if (x < 0 && y < 0)
+        return 3;
+    else if (x > 0 && y < 0)
+        return 4;
+
+    return 0;
+}
+System.Console.WriteLine(quarter);
+System.Console.WriteLine(FindQuarter(-19,7));
+System.Console.WriteLine(FindQuarter(-1,7));
+System.Console.WriteLine(FindQuarter(-9,-7));
+System.Console.WriteLine(FindQuarter(-5,4));
